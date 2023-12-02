8a351e3819d3f0b6350cbe5c47c0e526a3ec9825
Tony Wang <Tony94042@gmail.com>
2023-12-02 14:11:50 +1100

    Basic Setup Done

--- build/index.js
+++ build/index.js
@@ -1,6 +1,7 @@
 "use strict";
 console.log("Hello");
 console.log("TSC TEST");
+console.log("Compiling Test");
 const typeSomething = () => {
     console.log('Please Work');
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 console.log("Hello")
 console.log("TSC TEST")
+console.log("Compiling Test")
 
 
 const typeSomething = () => {
